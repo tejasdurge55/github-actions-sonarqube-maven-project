d6d092b7ffcf7ba2f3cd583584ddc79f4828494a
tejasdurge55 <tejas.y.durge@gmail.com>
2025-04-06 13:03:04 +0530

    updated text

--- src/main/java/com/example/webapp/HomeController.java
+++ src/main/java/com/example/webapp/HomeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
     @GetMapping("/")
     public String home(Model model) {
-        model.addAttribute("message", "Welcome to the Simple Web App by Tejas!");
+        model.addAttribute("message", "Welcome to the Simple Web App by Tejas Durge!");
         return "index";
     }
 }
