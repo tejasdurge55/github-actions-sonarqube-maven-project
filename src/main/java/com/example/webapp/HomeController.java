853b97ac87ae4ab2b46611e0518d8c800a068c3f
tejasdurge55 <tejas.y.durge@gmail.com>
2025-04-06 11:54:15 +0530

    uploaded new webapp

---
+++ src/main/java/com/example/webapp/HomeController.java
@@ -0,0 +1,14 @@
+package com.example.webapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("message", "Welcome to the Simple Web App!");
+        return "index";
+    }
+}
